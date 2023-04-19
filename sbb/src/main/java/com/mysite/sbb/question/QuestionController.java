9e4fffde07317ef7116934b75dcfc5d9de25e21f
comput3202 <comput3203@gmail.com>
2023-04-19 17:46:38 +0900

    2023.04.19.wed

--- sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -1,15 +1,25 @@
 package com.mysite.sbb.question;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
 public class QuestionController {
 
+	
+		@Autowired
+		QuestionRepository questionRepository;
+	
 		@GetMapping("/question/list")
-		
-		public String list() {
+		public String list(Model model) {
+			List<Question> questionList=this.questionRepository.findAll(); //select * from question;
+			model.addAttribute("viewData",questionList); //modelandView랑 비슷
+			
 			return "question_list"; //템플릿 파일
 		}
 }
